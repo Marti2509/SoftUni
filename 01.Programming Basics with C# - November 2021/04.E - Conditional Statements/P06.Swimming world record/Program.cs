e7dab5a93facb445d79fed3353f6e6cfe6cb8a49
Martin Simov <martisimov259@gmail.com>
2022-03-24 15:09:56 +0200

    Added "04.E - Conditional Statements" HW

---
+++ 01.Programming Basics with C# - November 2021/04.E - Conditional Statements/P06.Swimming world record/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace P06.Swimming_world_record
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordOld = double.Parse(Console.ReadLine());
+            double meters = double.Parse(Console.ReadLine());
+            double oneMeterInSec = double.Parse(Console.ReadLine());
+
+            double allSec = meters * oneMeterInSec;
+            double fiveteenMetersSlow = (Math.Floor(meters / 15)) * 12.5;
+            double obshtoVreme = allSec + fiveteenMetersSlow;
+
+            if (obshtoVreme < recordOld)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {Math.Abs(obshtoVreme):f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {Math.Abs(recordOld - obshtoVreme):f2} seconds slower.");
+            }
+        }
+    }
+}
---
+++ 01.Programming Basics with C# - November 2021/04.E - Conditional Statements/P07.Shopping/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P07.Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double buget = double.Parse(Console.ReadLine());
+            int videocarti = int.Parse(Console.ReadLine());
+            int procesori = int.Parse(Console.ReadLine());
+            int ramPament = int.Parse(Console.ReadLine());
+
+
+            double videocartiPr = videocarti * 250;
+            double procesoriPrice = videocartiPr * 0.35;
+            double procesoriPr = procesoriPrice * procesori;
+            double ramPametPrice = videocartiPr * 0.1;
+            double ramPametPr = ramPametPrice * ramPament;
+            double obshto = videocartiPr + procesoriPr + ramPametPr;
+
+            if (videocarti > procesori)
+            {
+                obshto = obshto - obshto * 0.15;
+            }
+
+            if (obshto <= buget)
+            {
+                Console.WriteLine($"You have {Math.Abs(obshto - buget):f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {Math.Abs(buget - obshto):f2} leva more!");
+            }
+        }
+    }
+}
