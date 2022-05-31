4d7fe0eedb1129eddf93eb51622e38412f0736cd
Martin Simov <martisimov259@gmail.com>
2022-05-31 18:15:32 +0300

    Added "08.1.Associative Arrays" HW

---
+++ 02.Programming Fundamentals with C# - January 2022/08.1.Associative Arrays/P01.Count Real Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01.Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> dictionary = new SortedDictionary<int, int>();
+
+            foreach (int number in numbers)
+            {
+                if (dictionary.ContainsKey(number))
+                {
+                    dictionary[number]++;
+                }
+                else
+                {
+                    dictionary.Add(number, 1);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
---
+++ 02.Programming Fundamentals with C# - January 2022/08.1.Associative Arrays/P02.Odd Occurrences/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (string wordOrigin in text)
+            {
+                string word = wordOrigin.ToLower();
+
+                if (words.ContainsKey(word))
+                {
+                    words[word]++;
+                }
+                else
+                {
+                    words.Add(word, 1);
+                }
+            }
+
+            foreach (var item in words)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{ item.Key} ");
+                }
+            }
+        }
+    }
+}
