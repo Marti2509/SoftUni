ac851489db38dc053b54c87d7d031d816c9085fd
Martin Simov <martisimov259@gmail.com>
2022-06-01 16:24:53 +0300

    Added "10.1.Regular Expressions" HW

---
+++ 02.Programming Fundamentals with C# - January 2022/10.1.Regular Expressions/P01.Match Full Name/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace P01.Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+            string names = Console.ReadLine();
+
+            List<string> sucNames = new List<string>();
+
+            //foreach (string name in names)
+            //{
+            //    Match match = Regex.Match(name, pattern);
+
+            //    if (match.Success)
+            //    {
+            //        sucNames.Add(match.Value);
+            //    }
+            //}
+
+            MatchCollection matches = Regex.Matches(names, pattern);
+
+            Console.WriteLine(string.Join(' ', matches));
+        }
+    }
+}
---
+++ 02.Programming Fundamentals with C# - January 2022/10.1.Regular Expressions/P03.Match Dates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P03.Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<day>\d{2})(.)(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})";
+
+            string phones = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(phones, pattern);
+
+            foreach (Match m in matches)
+            {
+                Console.WriteLine($"Day: {m.Groups["day"]}, Month: {m.Groups["month"]}, Year: {m.Groups["year"]}");
+            }
+        }
+    }
+}
