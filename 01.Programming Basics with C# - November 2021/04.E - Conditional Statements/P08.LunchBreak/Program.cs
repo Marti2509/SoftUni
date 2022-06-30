0fbfd10d884c8a359e73e9389e13c6f461e20a0e
Martin Simov <martisimov259@gmail.com>
2022-06-30 18:06:38 +0300

    Updated "04.E-ConditionalStatements" 2.0

---
+++ 01.Programming Basics with C# - November 2021/04.E - Conditional Statements/P08.LunchBreak/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace P08.Lunch_break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string serialName = Console.ReadLine();
+            int epizode = int.Parse(Console.ReadLine());
+            int pochivkaTime = int.Parse(Console.ReadLine());
+
+            double vremeZaObqd = pochivkaTime * 0.125;
+            double vremeZaOtdih = pochivkaTime * 0.25;
+            double ostavashtoVreme = pochivkaTime - vremeZaObqd - vremeZaOtdih;
+
+            if (ostavashtoVreme >= epizode)
+            {
+                double obshto = Math.Ceiling(ostavashtoVreme - epizode);
+                Console.WriteLine($"You have enough time to watch {serialName} and left with {Math.Abs(obshto)} minutes free time.");
+            }
+            else
+            {
+                double obshto = Math.Ceiling(epizode - ostavashtoVreme);
+                Console.WriteLine($"You don't have enough time to watch {serialName}, you need {Math.Abs(obshto)} more minutes.");
+            }
+        }
+    }
+}
