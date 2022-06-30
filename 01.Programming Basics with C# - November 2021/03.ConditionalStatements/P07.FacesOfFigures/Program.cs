c45635e381074457b2727ab740bc0eaac168cf71
Martin Simov <martisimov259@gmail.com>
2022-06-30 18:06:12 +0300

    Updated "03.ConditionalStatements" 2.0

---
+++ 01.Programming Basics with C# - November 2021/03.ConditionalStatements/P07.FacesOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace P07.Faces_Of_Figures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = a * a;
+            }
+            else if (figure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+            }
+            else if (figure == "circle")
+            {
+                double r = double.Parse(Console.ReadLine());
+                area = r * r * Math.PI;
+            }
+            else if (figure == "triangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double ha = double.Parse(Console.ReadLine());
+                area = a * ha / 2;
+            }
+
+            Console.WriteLine($"{area:f3}");
+        }
+    }
+}
