9746211b71da8c1c528338e09e52a748e0d1127b
Martin Simov <martisimov259@gmail.com>
2022-07-20 17:16:04 +0300

    Updated "05.NestedConditionalStatements" 2.0

---
+++ 01.Programming Basics with C# - November 2021/05.NestedConditionalStatements/P11.FruitStore/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace P11.Fruit_store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+            if (dayOfWeek == "Monday" ||
+                dayOfWeek == "Tuesday" ||
+                dayOfWeek == "Wednesday" ||
+                dayOfWeek == "Thursday" ||
+                dayOfWeek == "Friday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.50;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.20;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.85;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.45;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.50;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 3.85;
+                }
+            }
+            else if (dayOfWeek == "Saturday" ||
+                     dayOfWeek == "Sunday")
+            {
+                if (fruit == "banana")
+                {
+                    price = 2.70;
+                }
+                else if (fruit == "apple")
+                {
+                    price = 1.25;
+                }
+                else if (fruit == "orange")
+                {
+                    price = 0.90;
+                }
+                else if (fruit == "grapefruit")
+                {
+                    price = 1.60;
+                }
+                else if (fruit == "kiwi")
+                {
+                    price = 3.00;
+                }
+                else if (fruit == "pineapple")
+                {
+                    price = 5.60;
+                }
+                else if (fruit == "grapes")
+                {
+                    price = 4.20;
+                }
+            }
+
+            double priceQuantity = price * quantity;
+
+            if (priceQuantity > 0)
+            {
+                Console.WriteLine($"{priceQuantity:f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
