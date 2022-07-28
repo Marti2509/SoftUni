5b86d8c265fdd72247dfb16b2f694c6292e5659c
Marti2509 <martisimov259@gmail.com>
2022-07-28 15:25:25 +0300

    Added "10.1.IteratorsAndComparators" HW

---
+++ 03.C# Advanced - May 2022/01.Advanced/10.1.IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+            //this.books.Sort();
+            this.books.Sort(new BookComparator());
+        }
+
+        private List<Book> books;
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            // default:
+            //      return Books.GetEnumerator();
+            //
+            // mine:
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+            private int index;
+
+            public LibraryIterator(List<Book> books)
+            {
+                Reset();
+                this.books = books;
+            }
+
+            public Book Current => books[index];
+
+            public bool MoveNext()
+            {
+                index++;
+                return index < books.Count;
+
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
+
+            // legacy
+            object IEnumerator.Current => Current;
+
+            // don't need
+            public void Dispose() { }
+
+        }
+    }
+}
---
+++ 03.C# Advanced - May 2022/01.Advanced/10.1.IteratorsAndComparators/IteratorsAndComparators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IteratorsAndComparators
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Book bookOne = new Book("a", 2000, "someone");
+            Book bookTwo = new Book("b", 2002,
+                "someone", "someone");
+            Book bookThree = new Book("a", 2001);
+
+            Library library = new Library(bookOne, bookTwo, bookThree);
+
+            foreach (var book in library)
+            {
+                Console.WriteLine(book);
+            }
+
+        }
+    }
+}
