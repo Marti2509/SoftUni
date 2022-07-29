a5eafe22fe6ef7a0d67657cc0b4d2afe86968ff9
Marti2509 <martisimov259@gmail.com>
2022-07-29 23:55:42 +0300

    Added "01.2.E-Inheritance" HW without "P05.Restaurant"

---
+++ 03.C# Advanced - May 2022/02.OOP/01.2.E-Inheritance/P01.Person/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        public Child(string name, uint age)
+            : base(name, age)
+        {
+
+        }
+    }
+}
