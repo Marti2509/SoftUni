081cfcdaca23f26eb120ea43844483e28b0da176
Marti2509 <martisimov259@gmail.com>
2022-07-29 22:00:25 +0300

    Added "01.1.Inheritance"

---
+++ 03.C# Advanced - May 2022/02.OOP/01.1.Inheritance/P02.MultipleInheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Puppy puppy = new Puppy();
+
+            puppy.Bark();
+            puppy.Eat();
+            puppy.Weep();
+        }
+    }
+}
---
+++ 03.C# Advanced - May 2022/02.OOP/01.1.Inheritance/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public Stack<string> AddRange(params string[] items)
+        {
+            foreach (string item in items)
+            {
+                this.Push(item);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            return this;
+        }
+    }
+}
