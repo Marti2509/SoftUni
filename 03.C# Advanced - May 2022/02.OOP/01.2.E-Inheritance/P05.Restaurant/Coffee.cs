bf3dfc628299c62c244b89d0d308c06b6d1507fa
Marti2509 <martisimov259@gmail.com>
2022-07-30 22:19:58 +0300

    Added "02.1.Encapsulation" HW

---
+++ 03.C# Advanced - May 2022/02.OOP/01.2.E-Inheritance/P05.Restaurant/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        public Coffee(string name, double coffeine)
+            : base(name, 3.50m, 50.00)
+        {
+            Coffeine = coffeine;
+        }
+
+        public double Coffeine { get; set; }
+    }
+}
---
+++ 03.C# Advanced - May 2022/02.OOP/02.1.Encapsulation/P02.SalaryIncrease/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public decimal Salary { get; set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+                Salary += (Salary * percentage / 100) / 2;
+            else
+                Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
