51663ba150cc30dcbe15cac7a82cba3138485380
Martin Simov <martisimov259@gmail.com>
2022-08-02 17:39:54 +0300

    Added "03.1.InterfacesАndAbstraction" HW

---
+++ 03.C# Advanced - May 2022/02.OOP/03.1.InterfacesАndAbstraction/P02.Cars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cars
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ICar seat = new Seat("Leon", "Grey");
+            Console.WriteLine(seat.Start());
+            Console.WriteLine(seat.Stop());
+            ICar tesla = new Tesla("Model 3", "Red", 2);
+            Console.WriteLine(tesla.Start());
+            Console.WriteLine(tesla.Stop());
+
+            Console.WriteLine(seat);
+            Console.WriteLine(tesla);
+        }
+    }
+}
