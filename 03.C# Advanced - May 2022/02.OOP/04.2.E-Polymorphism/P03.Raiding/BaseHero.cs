6a74756dc7dbf9bb8c62d1619bf021203cba2708
Marti2509 <martisimov259@gmail.com>
2022-08-10 18:34:14 +0300

    Added "P03.Raiding" To "04.2.E-Polymorphism" HW

---
+++ 03.C# Advanced - May 2022/02.OOP/04.2.E-Polymorphism/P03.Raiding/BaseHero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+        private string name;
+        private int power;
+
+        public BaseHero(string name, int power)
+        {
+            Name = name;
+            Power = power;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+
+        public int Power
+        {
+            get { return power; }
+            private set { power = value; }
+        }
+
+        public abstract string CastAbility();
+    }
+}
---
+++ 03.C# Advanced - May 2022/02.OOP/04.2.E-Polymorphism/P03.Raiding/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Raiding
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<BaseHero> heros = new List<BaseHero>();
+
+            while (n > heros.Count)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+
+                if (type == "Druid")
+                    heros.Add(new Druid(name));
+                else if (type == "Paladin")
+                    heros.Add(new Paladin(name));
+                else if (type == "Rogue")
+                    heros.Add(new Rogue(name));
+                else if (type == "Warrior")
+                    heros.Add(new Warrior(name));
+                else
+                    Console.WriteLine("Invalid hero!");
+            }
+
+            int heroPoints = 0;
+
+            foreach (BaseHero hero in heros)
+            {
+                Console.WriteLine(hero.CastAbility());
+                heroPoints += hero.Power;
+            }
+
+            int bossPoints = int.Parse(Console.ReadLine());
+
+            if (heroPoints >= bossPoints)
+                Console.WriteLine("Victory!");
+            else
+                Console.WriteLine("Defeat...");
+        }
+    }
+}
