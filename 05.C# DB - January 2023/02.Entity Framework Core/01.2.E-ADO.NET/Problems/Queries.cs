cec9b72b42b6f2196aa03900ea81eb8ea596c8f8
Marti2509 <martisimov259@gmail.com>
2023-02-23 22:19:50 +0200

    Added the first 3 Problems to "01.2.E-ADO.NET" HW

---
+++ 05.C# DB - January 2023/02.Entity Framework Core/01.2.E-ADO.NET/Problems/Queries.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    internal class Queries
+    {
+        public const string villainNamesSQL = @"SELECT v.Name" +
+            "                        ,COUNT(mv.VillainId) AS MinionsCount" +
+            "                    FROM Villains AS v" +
+            "                    JOIN MinionsVillains AS mv ON v.Id = mv.VillainId" +
+            "                   GROUP BY v.Id" +
+            "                           ,v.Name" +
+            "                  HAVING COUNT(mv.VillainId) > 3" +
+            "                   ORDER BY COUNT(mv.VillainId)";
+
+        public const string minionNamesSQL1 = @"SELECT Name FROM Villains WHERE Id = @Id";
+
+        public const string minionNamesSQL2 = @"SELECT ROW_NUMBER() OVER (ORDER BY m.Name) AS RowNum,
+                                                       m.Name, 
+                                                       m.Age
+                                                  FROM MinionsVillains AS mv
+                                                  JOIN Minions As m ON mv.MinionId = m.Id
+                                                 WHERE mv.VillainId = @Id
+                                                 ORDER BY m.Name";
+
+        public const string addMinion1 = @"SELECT Id FROM Towns WHERE Name = @townName";
+
+        public const string addMinion2 = @"SELECT Id FROM Villains WHERE Name = @Name";
+
+        public const string addMinion3 = @"SELECT Id FROM Minions WHERE Name = @Name";
+                            
+        public const string addMinion4 = @"INSERT INTO Towns (Name) VALUES (@townName)";
+                            
+        public const string addMinion5 = @"INSERT INTO Villains (Name, EvilnessFactorId)  VALUES (@villainName, 4)";
+                            
+        public const string addMinion6 = @"INSERT INTO Minions (Name, Age, TownId) VALUES (@name, @age, @townId)";
+                            
+        public const string addMinion7 = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@minionId, @villainId)";
+    }
+}
---
+++ 05.C# DB - January 2023/02.Entity Framework Core/01.2.E-ADO.NET/Problems/StartUp.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace Problems
+{
+    public class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            using SqlConnection connection = new SqlConnection(Config.sqlConnectionData);
+            await connection.OpenAsync();
+
+            string result = await AddMinion(connection);
+            Console.WriteLine(result);
+        }
+
+        // PROBLEM 2
+        async static Task<string> VillainNames(SqlConnection connection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            SqlCommand command = new SqlCommand(Queries.villainNamesSQL, connection);
+
+            SqlDataReader reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                string name = (string)reader["Name"];
+                int count = (int)reader["MinionsCount"];
+
+                sb.Append(name + " - " + count);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        // PROBLEM 3
+        static async Task<string> MinionNames(SqlConnection connection)
+        {
+            int id = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+
+            SqlCommand commandName = new SqlCommand(Queries.minionNamesSQL1, connection);
+            commandName.Parameters.AddWithValue("@Id", id);
+
+            object? villian = await commandName.ExecuteScalarAsync();
+
+            if (villian == null)
+            {
+                return $"No villain with ID {id} exists in the database.";
+            }
+
+            sb.AppendLine($"Villain: {villian}");
+
+            SqlCommand commandMinions = new SqlCommand(Queries.minionNamesSQL2, connection);
+            commandMinions.Parameters.AddWithValue("@Id", id);
+
+            SqlDataReader reader = await commandMinions.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                long rowNumber = (long)reader["RowNum"];
+                string minionName = (string)reader["Name"];
+                int minionAge = (int)reader["Age"];
+
+                sb.AppendLine($"{rowNumber}. {minionName} {minionAge}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        //PROBLEM 4
+        static async Task<string> AddMinion(SqlConnection connection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            //"Minion: <Name> <Age> <TownName>"
+
+            string[] minionInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string minionName = minionInput[1];
+            int minionAge = int.Parse(minionInput[2]);
+            string minionTown = minionInput[3];
+
+            //"Villain: <Name>"
+
+            string[] villainInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string villainName = villainInput[1];
+
+            SqlCommand townCommand = new SqlCommand(Queries.addMinion1, connection);
+            townCommand.Parameters.AddWithValue("@townName", minionTown);
+
+            object? minionTownId = await townCommand.ExecuteScalarAsync();
+
+            if (minionTownId == null)
+            {
+                SqlCommand addTownCommand = new SqlCommand(Queries.addMinion4, connection);
+                addTownCommand.Parameters.AddWithValue("@townName", minionTown);
+
+                await addTownCommand.ExecuteNonQueryAsync();
+
+                sb.AppendLine($"Town {minionTown} was added to the database.");
+
+                minionTownId = await townCommand.ExecuteScalarAsync();
+            }
+
+            SqlCommand villainCommand = new SqlCommand(Queries.addMinion2, connection);
+            villainCommand.Parameters.AddWithValue("@Name", villainName);
+
+            object? villainId = await villainCommand.ExecuteScalarAsync();
+
+            if (villainId == null)
+            {
+                SqlCommand addVillainCommand = new SqlCommand(Queries.addMinion5, connection);
+                addVillainCommand.Parameters.AddWithValue("villainName", villainName);
+
+                await addVillainCommand.ExecuteNonQueryAsync();
+
+                sb.AppendLine($"Villain {villainName} was added to the database.");
+
+                villainId = await villainCommand.ExecuteScalarAsync();
+            }
+
+            SqlCommand minionCommand = new SqlCommand(Queries.addMinion3, connection);
+            minionCommand.Parameters.AddWithValue("@Name", minionName);
+
+            object? minionId = await villainCommand.ExecuteScalarAsync();
+
+            if (minionId == null)
+            {
+                SqlCommand addMinionCommand = new SqlCommand(Queries.addMinion6, connection);
+                addMinionCommand.Parameters.AddWithValue("@name", minionName);
+                addMinionCommand.Parameters.AddWithValue("@age", minionAge);
+                addMinionCommand.Parameters.AddWithValue("@town", minionTownId);
+
+                await addMinionCommand.ExecuteNonQueryAsync();
+
+                minionId = await villainCommand.ExecuteScalarAsync();
+            }
+
+            SqlCommand addToMinionsVillains = new SqlCommand(Queries.addMinion7, connection);
+            addToMinionsVillains.Parameters.AddWithValue("minionId", minionId);
+            addToMinionsVillains.Parameters.AddWithValue("villainId", villainId);
+
+            await addToMinionsVillains.ExecuteNonQueryAsync();
+
+            sb.AppendLine($"Successfully added {minionName} to be minion of {villainName}.");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
