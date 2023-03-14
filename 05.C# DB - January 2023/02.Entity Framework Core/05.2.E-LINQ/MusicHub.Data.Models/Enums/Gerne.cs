b9c918438578e1b7b8f877317a47c9a32a24752b
Marti2509 <martisimov259@gmail.com>
2023-03-14 10:33:23 +0200

    Added the First Problem to "05.2.E-LINQ" HW

---
+++ 05.C# DB - January 2023/02.Entity Framework Core/05.2.E-LINQ/MusicHub.Data.Models/Enums/Gerne.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicHub.Data.Models.Enums
+{
+    public enum Gerne
+    {
+        Blues = 0,
+        Rap = 1,
+        PopMusic = 2,
+        Rock = 3,
+        Jazz = 4
+    }
+}
---
+++ 05.C# DB - January 2023/02.Entity Framework Core/05.2.E-LINQ/MusicHub/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace MusicHub;
+
+using MusicHub.Data;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        MusicHubDbContext context =
+            new MusicHubDbContext();
+
+
+    }
+
+    public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+    {
+        throw new NotImplementedException();
+    }
+}
