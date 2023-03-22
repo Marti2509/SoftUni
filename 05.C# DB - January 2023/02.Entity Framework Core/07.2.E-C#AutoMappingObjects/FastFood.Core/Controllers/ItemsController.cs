cd2d8be268fa4068c7270a7465666df01949f36d
Marti2509 <martisimov259@gmail.com>
2023-03-22 11:54:03 +0200

    Added "07.2.E-C#AutoMappingObjects" HW

---
+++ 05.C# DB - January 2023/02.Entity Framework Core/07.2.E-C#AutoMappingObjects/FastFood.Core/Controllers/ItemsController.cs
@@ -0,0 +1,55 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Core.ViewModels.Positions;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Items;
+
+    public class ItemsController : Controller
+    {
+        private readonly FastFoodContext _context;
+        private readonly IMapper _mapper;
+
+        public ItemsController(FastFoodContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateItemInputModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            var item = _mapper.Map<Item>(model);
+
+            _context.Items.Add(item);
+
+            _context.SaveChanges();
+
+            return RedirectToAction("All", "Items");
+        }
+
+        public IActionResult All()
+        {
+            var items = _context.Items
+                .ProjectTo<ItemsAllViewModels>(_mapper.ConfigurationProvider)
+                .ToList();
+
+            return View(items);
+        }
+    }
+}
---
+++ 05.C# DB - January 2023/02.Entity Framework Core/07.2.E-C#AutoMappingObjects/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,36 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Models;
+
+    using ViewModels.Categories;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(m => m.Name, opt => opt.MapFrom(p => p.PositionName));
+
+            CreateMap<Position, PositionsAllViewModel>();
+
+            //Categories
+            CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(m => m.Name, opt => opt.MapFrom(c => c.CategoryName));
+
+            CreateMap<Category, CategoryAllViewModel>();
+
+            CreateMap<Category, CreateItemViewModel>()
+                .ForMember(d => d.CategoryId, opt => opt.MapFrom(src => src.Id));
+
+            //Items
+            CreateMap<CreateItemInputModel, Item>();
+
+            CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(d => d.Category, opt => opt.MapFrom(src => src.Category.Name));
+        }
+    }
+}
