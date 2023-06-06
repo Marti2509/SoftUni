256c56d709bf6e06556affc1cfa331b59bdec5ff
Martin Simov <martisimov259@gmail.com>
2023-06-06 11:03:06 +0300

    Added the Demo to "01.2.E-ASP.NETCoreIntroduction" HW

---
+++ 06.C# Web - May 2023/01.ASP.NET Fundamentals/01.2.E-ASP.NETCoreIntroduction/DemoASPNET/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using DemoASPNET.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoASPNET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Home page";
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is the ASP.NET Core MVC Demo app.";
+            ViewBag.Creator = "Martin Simov";
+            return View();
+        }
+
+        public IActionResult Numbers1To50()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult NumbersToN()
+        {
+            ViewBag.Count = 3;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            return View();
+        }
+
+        [Route("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 06.C# Web - May 2023/01.ASP.NET Fundamentals/01.2.E-ASP.NETCoreIntroduction/DemoASPNET/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using DemoASPNET.Models.Product;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DemoASPNET.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IEnumerable<ProductViewModel> _products = new List<ProductViewModel>()
+        { 
+            new ProductViewModel()
+            {
+                Id = 1,
+                Name = "Cheese",
+                Price = 6.80m
+            },
+            new ProductViewModel()
+            {
+                Id = 2,
+                Name = "Ham",
+                Price = 5.50m
+            },
+            new ProductViewModel()
+            {
+                Id = 3,
+                Name = "Bread",
+                Price = 1.99m
+            },
+            new ProductViewModel()
+            {
+                Id = 4,
+                Name = "Ice Cream",
+                Price = 5.99m
+            }
+        };
+
+        public IActionResult AllProducts(string keyword)
+        {
+            if (keyword != null)
+            {
+                var products = _products.Where(x => x.Name.ToLower().Contains(keyword.ToLower()));
+                return View(products);
+            }
+
+            return View(_products);
+        }
+
+        public IActionResult ProductById(int id)
+        {
+            var product = _products.FirstOrDefault(x => x.Id == id);
+
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult AllProductsAsJson()
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+
+            return Json(_products, options);
+        }
+
+        public IActionResult AllProductsAsText()
+        {
+            var text = string.Empty;
+            foreach (var product in _products)
+            {
+                text += $"Product {product.Id}: {product.Name} - {product.Price}lv.";
+                text += "\r\n";
+            }
+            return Content(text);
+        }
+
+        public IActionResult AllProductsAsTextFile()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var product in _products)
+            {
+                sb.AppendLine($"Product {product.Id}: {product.Name} - {product.Price}lv.");
+            }
+
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+
+            return File(Encoding.UTF8.GetBytes(sb.ToString().Trim()), "text/plain");
+        }
+
+
+    }
+}
