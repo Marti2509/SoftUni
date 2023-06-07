507706c3cb05405d2cc537e874a026fc4af1df5c
Martin Simov <martisimov259@gmail.com>
2023-06-07 08:59:58 +0300

    Added the ChatApp to "01.2.E-ASP.NETCoreIntroduction" HW

---
+++ 06.C# Web - May 2023/01.ASP.NET Fundamentals/01.2.E-ASP.NETCoreIntroduction/ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,42 @@
+﻿using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static ICollection<KeyValuePair<string, string>> _messages = new List<KeyValuePair<string, string>>();
+
+        [HttpGet]
+        public IActionResult Show()
+        {
+            if (_messages.Count == 0)
+            {
+                return View(new ChatViewModel());
+            }
+
+            ChatViewModel chatModel = new ChatViewModel()
+            {
+                Messages = _messages.Select(m => new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    Message = m.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat)
+        {
+            var newMessage = chat.CurrentMessage;
+
+            _messages.Add(new KeyValuePair<string, string>(newMessage.Sender, newMessage.Message));
+
+            return RedirectToAction("Show");
+        }
+    }
+}
---
+++ 06.C# Web - May 2023/01.ASP.NET Fundamentals/01.2.E-ASP.NETCoreIntroduction/ChatApp/Models/Message/MessageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models.Message
+{
+    public class MessageViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string Sender { get; set; } = null!;
+
+        [Required]
+        [StringLength(200)]
+        public string Message { get; set; } = null!;
+    }
+}
